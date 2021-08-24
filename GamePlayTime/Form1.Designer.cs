b7b159ebd5ab34a30499454447a88c84e172b738
Benjamin Daly <mvben33@gmail.com>
2021-08-23 19:13:47 -0700

    Basic app tracking/hiding functionality intact

--- GamePlayTime/Form1.Designer.cs
+++ GamePlayTime/Form1.Designer.cs
@@ -31,47 +31,64 @@ namespace GamePlayTime
         {
             this.components = new System.ComponentModel.Container();
             this.AllProcessesBox = new System.Windows.Forms.ListBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AllExecutableContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.trackProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hideProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.TrackedProcessesBox = new System.Windows.Forms.ListBox();
-            this.contextMenuStrip1.SuspendLayout();
+            this.TrackedExecutableContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stopTrackingProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideProcessToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideProcessToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.showHiddenProcessesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openTrackedProcessesFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openHiddenProcessesFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AllExecutableContextMenuStrip.SuspendLayout();
+            this.TrackedExecutableContextMenuStrip.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // AllProcessesBox
             // 
             this.AllProcessesBox.CausesValidation = false;
             this.AllProcessesBox.FormattingEnabled = true;
-            this.AllProcessesBox.Location = new System.Drawing.Point(12, 28);
-            this.AllProcessesBox.MaximumSize = new System.Drawing.Size(251, 316);
-            this.AllProcessesBox.MinimumSize = new System.Drawing.Size(251, 40);
+            this.AllProcessesBox.ItemHeight = 16;
+            this.AllProcessesBox.Location = new System.Drawing.Point(16, 79);
+            this.AllProcessesBox.Margin = new System.Windows.Forms.Padding(4);
+            this.AllProcessesBox.MaximumSize = new System.Drawing.Size(333, 388);
+            this.AllProcessesBox.MinimumSize = new System.Drawing.Size(333, 48);
             this.AllProcessesBox.Name = "AllProcessesBox";
-            this.AllProcessesBox.Size = new System.Drawing.Size(251, 316);
+            this.AllProcessesBox.Size = new System.Drawing.Size(333, 388);
             this.AllProcessesBox.TabIndex = 0;
             this.AllProcessesBox.TabStop = false;
-            this.AllProcessesBox.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.AllProcessesBox.Leave += new System.EventHandler(this.AllProcessesBox_Leave);
             this.AllProcessesBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
             // 
-            // contextMenuStrip1
+            // AllExecutableContextMenuStrip
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AllExecutableContextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.AllExecutableContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.trackProcessToolStripMenuItem,
             this.hideProcessToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.AllExecutableContextMenuStrip.Name = "contextMenuStrip1";
+            this.AllExecutableContextMenuStrip.Size = new System.Drawing.Size(166, 52);
             // 
             // trackProcessToolStripMenuItem
             // 
             this.trackProcessToolStripMenuItem.Name = "trackProcessToolStripMenuItem";
-            this.trackProcessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trackProcessToolStripMenuItem.Size = new System.Drawing.Size(165, 24);
             this.trackProcessToolStripMenuItem.Text = "Track Process";
             this.trackProcessToolStripMenuItem.Click += new System.EventHandler(this.trackProcessToolStripMenuItem_Click);
             // 
             // hideProcessToolStripMenuItem
             // 
             this.hideProcessToolStripMenuItem.Name = "hideProcessToolStripMenuItem";
-            this.hideProcessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hideProcessToolStripMenuItem.Size = new System.Drawing.Size(165, 24);
             this.hideProcessToolStripMenuItem.Text = "Hide Process";
             this.hideProcessToolStripMenuItem.Click += new System.EventHandler(this.hideProcessToolStripMenuItem_Click);
             // 
@@ -84,26 +101,137 @@ namespace GamePlayTime
             // TrackedProcessesBox
             // 
             this.TrackedProcessesBox.FormattingEnabled = true;
-            this.TrackedProcessesBox.Location = new System.Drawing.Point(269, 28);
-            this.TrackedProcessesBox.MaximumSize = new System.Drawing.Size(251, 316);
-            this.TrackedProcessesBox.MinimumSize = new System.Drawing.Size(251, 40);
+            this.TrackedProcessesBox.ItemHeight = 16;
+            this.TrackedProcessesBox.Location = new System.Drawing.Point(359, 79);
+            this.TrackedProcessesBox.Margin = new System.Windows.Forms.Padding(4);
+            this.TrackedProcessesBox.MaximumSize = new System.Drawing.Size(333, 388);
+            this.TrackedProcessesBox.MinimumSize = new System.Drawing.Size(333, 48);
             this.TrackedProcessesBox.Name = "TrackedProcessesBox";
-            this.TrackedProcessesBox.Size = new System.Drawing.Size(251, 316);
+            this.TrackedProcessesBox.Size = new System.Drawing.Size(333, 388);
             this.TrackedProcessesBox.TabIndex = 1;
             this.TrackedProcessesBox.TabStop = false;
+            this.TrackedProcessesBox.Leave += new System.EventHandler(this.TrackedProcessesBox_Leave);
+            this.TrackedProcessesBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TrackedProcessesBox_MouseDown);
+            // 
+            // TrackedExecutableContextMenuStrip
+            // 
+            this.TrackedExecutableContextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.TrackedExecutableContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopTrackingProcessToolStripMenuItem});
+            this.TrackedExecutableContextMenuStrip.Name = "TrackedExecutableContextMenuStrip";
+            this.TrackedExecutableContextMenuStrip.Size = new System.Drawing.Size(222, 28);
+            // 
+            // stopTrackingProcessToolStripMenuItem
+            // 
+            this.stopTrackingProcessToolStripMenuItem.Name = "stopTrackingProcessToolStripMenuItem";
+            this.stopTrackingProcessToolStripMenuItem.Size = new System.Drawing.Size(221, 24);
+            this.stopTrackingProcessToolStripMenuItem.Text = "Stop Tracking Process";
+            this.stopTrackingProcessToolStripMenuItem.Click += new System.EventHandler(this.stopTrackingProcessToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "All Processes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(356, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tracked Processes";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(707, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openTrackedProcessesFileToolStripMenuItem,
+            this.openHiddenProcessesFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // processesToolStripMenuItem
+            // 
+            this.processesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hideProcessToolStripMenuItem1,
+            this.hideProcessToolStripMenuItem2,
+            this.showHiddenProcessesToolStripMenuItem});
+            this.processesToolStripMenuItem.Name = "processesToolStripMenuItem";
+            this.processesToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.processesToolStripMenuItem.Text = "Processes";
+            // 
+            // hideProcessToolStripMenuItem1
+            // 
+            this.hideProcessToolStripMenuItem1.Name = "hideProcessToolStripMenuItem1";
+            this.hideProcessToolStripMenuItem1.Size = new System.Drawing.Size(257, 26);
+            this.hideProcessToolStripMenuItem1.Text = "Track Process";
+            // 
+            // hideProcessToolStripMenuItem2
+            // 
+            this.hideProcessToolStripMenuItem2.Name = "hideProcessToolStripMenuItem2";
+            this.hideProcessToolStripMenuItem2.Size = new System.Drawing.Size(257, 26);
+            this.hideProcessToolStripMenuItem2.Text = "Hide Process";
+            // 
+            // showHiddenProcessesToolStripMenuItem
+            // 
+            this.showHiddenProcessesToolStripMenuItem.Name = "showHiddenProcessesToolStripMenuItem";
+            this.showHiddenProcessesToolStripMenuItem.Size = new System.Drawing.Size(257, 26);
+            this.showHiddenProcessesToolStripMenuItem.Text = "Show Hidden Processes...";
+            this.showHiddenProcessesToolStripMenuItem.Click += new System.EventHandler(this.showHiddenProcessesToolStripMenuItem_Click);
+            // 
+            // openTrackedProcessesFileToolStripMenuItem
+            // 
+            this.openTrackedProcessesFileToolStripMenuItem.Name = "openTrackedProcessesFileToolStripMenuItem";
+            this.openTrackedProcessesFileToolStripMenuItem.Size = new System.Drawing.Size(286, 26);
+            this.openTrackedProcessesFileToolStripMenuItem.Text = "Open Tracked Processes File...";
+            // 
+            // openHiddenProcessesFileToolStripMenuItem
+            // 
+            this.openHiddenProcessesFileToolStripMenuItem.Name = "openHiddenProcessesFileToolStripMenuItem";
+            this.openHiddenProcessesFileToolStripMenuItem.Size = new System.Drawing.Size(286, 26);
+            this.openHiddenProcessesFileToolStripMenuItem.Text = "Open Hidden Processes File...";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.ClientSize = new System.Drawing.Size(707, 480);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.TrackedProcessesBox);
             this.Controls.Add(this.AllProcessesBox);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Game Play Time";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.AllExecutableContextMenuStrip.ResumeLayout(false);
+            this.TrackedExecutableContextMenuStrip.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,10 +239,22 @@ namespace GamePlayTime
 
         private System.Windows.Forms.ListBox AllProcessesBox;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip AllExecutableContextMenuStrip;
         private System.Windows.Forms.ListBox TrackedProcessesBox;
         private System.Windows.Forms.ToolStripMenuItem hideProcessToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem trackProcessToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip TrackedExecutableContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem stopTrackingProcessToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openTrackedProcessesFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openHiddenProcessesFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideProcessToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem hideProcessToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem showHiddenProcessesToolStripMenuItem;
     }
 }
 
--- GamePlayTime/Form1.cs
+++ GamePlayTime/Form1.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using PInvoke;
 using System.Diagnostics;
-using System.Media;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
@@ -18,17 +13,20 @@ namespace GamePlayTime
 {
     public partial class Form1 : Form
     {
-        public bool ListsLocked { get; set; }
+        
         public string TrackedFileJsonPath { get; set; }
         public string HiddenFileJsonPath { get; set; }
         public static DateTime InitTime { get; set; }
 
-        private static List<Executable> allExecutable = new List<Executable>();
-        private static List<Executable> trackedExecutable = new List<Executable>();
-        private static List<Executable> hiddenExecutable = new List<Executable>();
+        public static List<Executable> AllExecutable = new List<Executable>();
+        public static List<Executable> TrackedExecutable = new List<Executable>();
+        public static List<Executable> HiddenExecutable = new List<Executable>();
+
+
+        private Form2 form2;
 
         [Serializable()]
-        private class Executable : ISerializable
+        public class Executable : ISerializable
         {
             public Process Process { get; }
             public string WindowTitle { get; }
@@ -79,8 +77,8 @@ namespace GamePlayTime
             TrackedFileJsonPath = "trackedexe.json";
             HiddenFileJsonPath = "hiddenexe.json";
 
-            ReadFromJson(TrackedFileJsonPath, out trackedExecutable);
-            ReadFromJson(HiddenFileJsonPath, out hiddenExecutable);
+            ReadFromJson(TrackedFileJsonPath, out TrackedExecutable);
+            ReadFromJson(HiddenFileJsonPath, out HiddenExecutable);
 
 
             RefreshAllWindows();
@@ -92,11 +90,11 @@ namespace GamePlayTime
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            LogToJson(TrackedFileJsonPath, trackedExecutable);
-            LogToJson(HiddenFileJsonPath, hiddenExecutable);
+            LogToJson(TrackedFileJsonPath, TrackedExecutable);
+            LogToJson(HiddenFileJsonPath, HiddenExecutable);
 
         }
-
+            
         private void timer1_Tick(object sender, EventArgs e)
         {
             RefreshAllWindows();
@@ -112,32 +110,42 @@ namespace GamePlayTime
         //Begin a way to track time based on the app, using its path as the identity.
         //Find a way to serialize the data.
 
-       
 
-        private void RefreshAllWindows()
+        private bool IsNotHiddenOrTracked(Executable ex)
         {
-            allExecutable.Clear();
+            return (!HiddenExecutable.Any(h => h.Path == ex.Path) && (!TrackedExecutable.Any(t => t.Path == ex.Path)));
+        }
+
+        private void CloseContextMenus()
+        {
+            AllExecutableContextMenuStrip.Close();
+            TrackedExecutableContextMenuStrip.Close();
+            if (form2 != null)
+                form2.CloseForm2ContextMenus();
+
+        }
+
+        public void RefreshAllWindows()
+        {
+            CloseContextMenus();
+            AllExecutable.Clear();
             AllProcessesBox.Items.Clear();
+            TrackedProcessesBox.Items.Clear();
             EnumDesktopWindows();
 
-            foreach(var aExe in allExecutable)
-            {   
-                foreach(var hExe in hiddenExecutable)
-                {
-                    if (aExe.Path == hExe.Path)
-                    {
-                        allExecutable.Remove(aExe);
-                    }
-                }
+            
+            if (HiddenExecutable != null)
+            {
+                AllExecutable = AllExecutable.Where(IsNotHiddenOrTracked).ToList();
             }
 
 
-            AddListToListbox(AllProcessesBox, allExecutable);
-            AddListToListbox(TrackedProcessesBox, trackedExecutable);
+            AddListToListbox(AllProcessesBox, AllExecutable);
+            AddListToListbox(TrackedProcessesBox, TrackedExecutable);
 
         }
         
-        private void AddListToListbox(ListBox lB, List<Executable> lEx)
+        public static void AddListToListbox(ListBox lB, List<Executable> lEx)
         {
             var strings = new List<string>();
             foreach (var exe in lEx)
@@ -162,45 +170,57 @@ namespace GamePlayTime
                 try
                 {
                     var process = Process.GetProcessById(pid);
-                    if (!string.IsNullOrWhiteSpace(process.MainWindowTitle))
-                        allExecutable.Add(new Executable(process, process.MainWindowTitle, process.MainModule.ModuleName, process.MainModule.FileName));
+
+                    bool Test1(Executable e)
+                    {
+                        return e.WindowTitle == "";
+                    }
+                    IEnumerable<Executable> stuff = AllExecutable.Where(Test1);
+                    bool xO = stuff.Any();
+
+                    if (!string.IsNullOrWhiteSpace(process.MainWindowTitle) && (!AllExecutable.Where(e => process.MainWindowTitle == e.WindowTitle).Any()))
+                        AllExecutable.Add(new Executable(process, process.MainWindowTitle, process.MainModule.ModuleName, process.MainModule.FileName));
                 }
                 catch
                 {
-                    SystemSounds.Hand.Play();
-                    MessageBox.Show("Something went wrong!");
+                    //SystemSounds.Hand.Play();
+                    //MessageBox.Show("Something went wrong!");
                 }
             }
             return true;
         }
 
-        private void listBox1_DoubleClick(object sender, EventArgs e)
-        {
-
-        }
 
         private void listBox1_MouseDown(object sender, MouseEventArgs e)
         {
+            RightClickGenericListBox(sender, e, AllExecutableContextMenuStrip);
+        }
+
+
+        public static void RightClickGenericListBox(object sender, MouseEventArgs e, ContextMenuStrip c)
+        {
             if (e.Button == MouseButtons.Right)
             {
-                var focusedItem = AllProcessesBox.SelectedItem;
-                var cursorPointIndex = AllProcessesBox.IndexFromPoint(e.Location);
+                var listBox = (ListBox)sender;
+                var cursorPointIndex = listBox.IndexFromPoint(e.Location);
+
+                listBox.Focus();
 
                 try
                 {
-                    AllProcessesBox.SetSelected(cursorPointIndex, true);
+                    listBox.SetSelected(cursorPointIndex, true);
                 }
                 catch
                 {
-
+                    MessageBox.Show("This is still an issue LOL");
                 }
 
+                var focusedItem = listBox.SelectedItem;
 
-
-                if (focusedItem != null && AllProcessesBox.SelectedIndex == cursorPointIndex && !(cursorPointIndex == -1) && AllProcessesBox.SelectedIndex != -1)
+                if (focusedItem != null && listBox.SelectedIndex == cursorPointIndex && !(cursorPointIndex == -1) && listBox.SelectedIndex != -1)
                 {
 
-                    contextMenuStrip1.Show(Cursor.Position);
+                    c.Show(Cursor.Position);
                 }
             }
         }
@@ -208,22 +228,32 @@ namespace GamePlayTime
         private void trackProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var focusedItem = AllProcessesBox.SelectedItem;
-            
+            var focusedIndex = AllProcessesBox.SelectedIndex;
+            var matchingItem = AllExecutable.ElementAt(focusedIndex);
+
+            AllProcessesBox.Items.Remove(focusedItem);
+            TrackedExecutable.Add(matchingItem);
+            AllExecutable.Remove(matchingItem);
+            TrackedProcessesBox.Items.Add(focusedItem);
+            Console.WriteLine("Just removed index " + focusedIndex + ", matching program " + matchingItem.ExecutableName);
         }
 
         private void hideProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            while (ListsLocked == true) { }
             var focusedItem = AllProcessesBox.SelectedItem;
             var focusedIndex = AllProcessesBox.SelectedIndex;
-            var matchingItem = allExecutable.ElementAt(focusedIndex);
+            var matchingItem = AllExecutable.ElementAt(focusedIndex);
 
             AllProcessesBox.Items.Remove(focusedItem);
-            allExecutable.Remove(matchingItem);
-            hiddenExecutable.Add(matchingItem);
-            Console.WriteLine("Just removed index " + focusedIndex + ", matching program " + matchingItem.ExecutableName);
-        }
+            HiddenExecutable.Add(matchingItem);
 
+            if (form2.Visible)
+                form2.AddHiddenToList(focusedItem);
+
+            AllExecutable.Remove(matchingItem);
+            Console.WriteLine("Just removed index " + focusedIndex + ", matching program " + matchingItem.ExecutableName);
+            RefreshAllWindows();
+        }
 
 
         private void LogToFile()
@@ -260,9 +290,15 @@ namespace GamePlayTime
 
         private void ReadFromJson(string filePath, out List<Executable> list)
         {
-            if (File.Exists(filePath))
+            //check if file is empty
+            if (File.Exists(filePath) && File.ReadAllBytes(filePath).Length  != 0)
             {
-                list = JsonConvert.DeserializeObject<List<Executable>>(File.ReadAllText(filePath));
+                list = JsonConvert.DeserializeObject<List<Executable>>(File.ReadAllText(filePath)).ToList();
+                //if (list == null)
+                //{
+                //    list = new List<Executable>();
+                //}
+                    
             }
             else
             {
@@ -270,10 +306,47 @@ namespace GamePlayTime
             }
         }
 
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
 
+        private void TrackedProcessesBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            RightClickGenericListBox(sender, e, TrackedExecutableContextMenuStrip);
+        }
+
+        private void AllProcessesBox_Leave(object sender, EventArgs e)
+        {
+            ListBox lB = (ListBox)sender;
+            lB.ClearSelected();
+        }
+
+        private void TrackedProcessesBox_Leave(object sender, EventArgs e)
+        {
+            ListBox lB = (ListBox)sender;
+            lB.ClearSelected();
+        }
+
+        private void stopTrackingProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var focusedItem = TrackedProcessesBox.SelectedItem;
+            var focusedIndex = TrackedProcessesBox.SelectedIndex;
+            var matchingItem = TrackedExecutable.ElementAt(focusedIndex);
+
+            TrackedProcessesBox.Items.Remove(focusedItem);
+            TrackedExecutable.Remove(matchingItem);
+            Console.WriteLine("Just removed index " + focusedIndex + ", matching program " + matchingItem.ExecutableName);
+        }
+
+        private void showHiddenProcessesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.Show();
+        }
     }
-
 }
+
 
 
 
